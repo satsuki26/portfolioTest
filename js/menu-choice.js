76b8c896e89879f2d44b2ddaa6ade90dd9df649c
satsuki26 <100519447+satsuki26@users.noreply.github.com>
2022-04-07 23:11:49 +0900

    Add files via upload

---
+++ js/menu-choice.js
@@ -0,0 +1,16 @@
+$(function(){
+  $("button").click(function(){
+    var target = $(this).attr("value");
+    
+    $("#list li").each(function(){
+      $(this).animate({"opacity": 0}, 300, function(){
+        $(this).hide();
+
+        if ($(this).hasClass(target) || target == "all") {
+          $(this).show();
+          $(this).animate({"opacity": 1}, 300);
+        }
+      });
+    });
+  });
+});
